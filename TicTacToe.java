3fb1ec6f38e66d4d5b71dfcca22b709a51a28c57
Hp <d1314995@yandex.ru>
2023-03-25 21:52:06 +0300

    add window

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,12 +1,14 @@
 import java.io.*;
 import java.util.*;
 import static java.lang.Math.*;
+import javax.swing.*;
+//import javax.swing.JWindow;
 
 public class TicTacToe{
-       public static void main(String[] args){
-
-
-
+        public static void main(String[] args){
+        JFrame window = new JFrame();
+        window.setVisible(true);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setSize(400, 300);
         }
-
-}
+}       
