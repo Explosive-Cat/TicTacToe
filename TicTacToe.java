877e4babf7c66471755549776897c9dd789438dc
Hp <d1314995@yandex.ru>
2023-03-26 17:02:59 +0300

    New Icon

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,14 +1,12 @@
-import java.io.*;
-import java.util.*;
-import static java.lang.Math.*;
 import javax.swing.*;
-//import javax.swing.JWindow;
 
 public class TicTacToe{
         public static void main(String[] args){
-        JFrame window = new JFrame();
+        JFrame window = new JFrame("TicTacToe");
+        ImageIcon icon = new ImageIcon("TTTicon.png");
         window.setVisible(true);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setSize(400, 300);
+        window.setIconImage(icon.getImage());
         }
 }       
